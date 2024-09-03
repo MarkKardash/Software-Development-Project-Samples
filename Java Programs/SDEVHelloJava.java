50d4c224cff9c23efc7c4dea0452082d8dc65518
MarkKardash <152450137+MarkKardash@users.noreply.github.com>
2024-09-03 14:09:09 -0400

    Add files via upload

---
+++ Java Programs/SDEVHelloJava.java
@@ -0,0 +1,13 @@
+/**
+ * Class HelloWorldJava
+ */
+public class SDEVHelloJava {
+	public static void main(String[] args) {
+    	System.out.println("Hello World!");
+    	System.out.println("My name is Mark Kardash!");
+    	System.out.println("This is a program to print out messages!");
+    	System.out.println("This class seems very interesting.");
+    	System.out.println("I can't wait to learn more!");
+
+	}
+}
