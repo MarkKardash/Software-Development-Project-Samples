8b314438690ff8c5b71ebbc6a5169a76845e7cdc
MarkKardash <152450137+MarkKardash@users.noreply.github.com>
2024-09-03 15:06:10 -0400

    Add files via upload

---
+++ SDEVHelloC++.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello World!" << std::endl;
+	std::cout << "My name is Mark Kardash!" <<std::endl;
+	std::cout << "This is a program to write sentences." <<std::endl;
+	std::cout << "This class seems interesting."  <<std::endl;
+	std::cout << "I can't wait to learn more." <<std::endl;
+	
+}
